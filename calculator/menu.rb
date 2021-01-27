c990c7c58775226ca49d692f33d7c8e6e7b9221a
Victor.Ehrich <victorehrich@gmail.com>
2021-01-27 20:13:54 -0300

    no_integers feito

--- calculator/menu.rb
+++ calculator/menu.rb
@@ -3,7 +3,29 @@ require_relative 'operations'
 module Calculator
   class Menu
     def initialize
+
+    puts "-------------------------\n| Bem vindo a calculadora |\n-------------------------"
+    puts "1. Operacao 1"
+    puts "2. Operacao 2"
+    puts "3. Sair"
+    puts "Sua opção: "
+    optionMenu = gets.chomp.to_i
     
+    case optionMenu
+      when 1
+        Operations.new
+        media = biased_mean()
+      when 2
+        numbers = gets.chomp
+        puts numbers[numbers.length - 2]
+        numero = Operations.new
+        puts numero.no_integers(numbers)
+                
+      when 3
+        
+      else
+        puts  "opcao invalida"
+      end
     end
   end
 end
--- calculator/operations.rb
+++ calculator/operations.rb
@@ -7,11 +7,28 @@ module Calculator
     include ExtraOperations
   
     def biased_mean(grades, blacklist)
+
     
     end
   
     def no_integers(numbers)
-  
+      
+        if numbers[numbers.length-2] == '0'
+          if numbers[numbers.length-3] == '5' || numbers[numbers.length-3] == '0'
+            numbers = 'S'
+          else
+            numbers = 'N'
+          end
+        elsif numbers[numbers.length-2] == '5'
+          if numbers[numbers.length-3] == '2' || numbers[numbers.length-3] == '7'
+            numbers = 'S'
+          else
+            numbers = 'N'
+          end
+        else
+          numbers = 'N'
+      end
+      
     end
   
     def filter_films(genres, year)
